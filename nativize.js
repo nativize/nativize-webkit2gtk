708f5de2e538a5c365830ba7c86a0703888f9e04
gardengim <gardengim@pm.me>
2025-02-23 16:00:14 +0900

    Add basic unimplemented functions and implement build

---
+++ nativize.js
@@ -0,0 +1,22 @@
+export const check = () => {}
+export const prepare = () => {}
+export const build = async () => {
+  try {
+    //setup buildsir
+    await new Deno.Command("meson", {
+      args: ["setup", "--reconfigure", "builddir"],
+      cwd: import.meta.dirname
+    }).spawn().status;
+    //compile
+    await new Deno.Command("meson", {
+      args: ["compile", "-C", "builddir"],
+      cwd: import.meta.dirname
+    }).spawn().status;
+  } catch (error) {
+    console.error(error);
+  }
+}
+export const run = () => {}
+export const clean = () => {}
+
+await build();
