2b9a0104bd446d31c2edfe6156fcf40f2a8031d2
gardengim <gardengim@pm.me>
2025-02-24 08:46:00 +0900

    Impl clean

--- nativize.js
+++ nativize.js
@@ -1,22 +1,29 @@
-export const check = () => {}
-export const prepare = () => {}
+export const check = () => {};
+export const prepare = () => {};
 export const build = async () => {
   try {
     //setup buildsir
     await new Deno.Command("meson", {
       args: ["setup", "--reconfigure", "builddir"],
-      cwd: import.meta.dirname
+      cwd: import.meta.dirname,
     }).spawn().status;
     //compile
     await new Deno.Command("meson", {
       args: ["compile", "-C", "builddir"],
-      cwd: import.meta.dirname
+      cwd: import.meta.dirname,
     }).spawn().status;
   } catch (error) {
     console.error(error);
   }
-}
-export const run = () => {}
-export const clean = () => {}
-
-await build();
+};
+export const run = () => {};
+export const clean = () => {
+  try {
+    await new Deno.Command("meson", {
+      args: ["compile", "--clean", "-C", "builddir"],
+      cwd: import.meta.dirname,
+    }).spawn().status;
+  } catch (error) {
+    console.error(error);
+  }
+};
